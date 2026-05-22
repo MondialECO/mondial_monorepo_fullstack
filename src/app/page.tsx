bef8a121f69daefd72bd8ebd25923d84cfa67e4e
Masud Reza <masudrezaog@gmail.com>
2026-05-22 14:25:08 +0600

    mitigate hydration mismatches from extension DOM mutations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
 import Navbar from "@/components/shared/Navbar";
 import HeroSection from "@/components/homepage/HeroSection";
 import AllProfileSection from "@/components/homepage/AllProfileSection";
