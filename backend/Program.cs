aa3871d14f42d13dafcf1494fb87789ebc415581
Siraj <mondialecoteam@gmail.com>
2026-09-16 01:17:10 +0600

    fix(di): register LogoMarkRendererRegistry concrete type and use interface in LogoVariationService

--- backend/Program.cs
+++ backend/Program.cs
@@ -325,8 +325,10 @@ builder.Services.AddSingleton<WebApp.Services.Repository.ICreatorIdeaStore>(
 builder.Services.AddSingleton<WebApp.Services.Repository.BrandKitRepository>();
 builder.Services.AddSingleton<WebApp.Services.Repository.IBrandKitStore>(
     sp => sp.GetRequiredService<WebApp.Services.Repository.BrandKitRepository>());
-builder.Services.AddSingleton<WebApp.Services.Creator.BrandKit.LogoEngine.ILogoMarkRendererRegistry,
-    WebApp.Services.Creator.BrandKit.LogoEngine.LogoMarkRendererRegistry>();
+builder.Services.AddSingleton<WebApp.Services.Creator.BrandKit.LogoEngine.LogoMarkRendererRegistry>();
+builder.Services.AddSingleton<WebApp.Services.Creator.BrandKit.LogoEngine.ILogoMarkRendererRegistry>(
+    sp => sp.GetRequiredService<WebApp.Services.Creator.BrandKit.LogoEngine.LogoMarkRendererRegistry>());
+
 builder.Services.AddScoped<WebApp.Services.Creator.BrandKit.LogoEngine.ILogoGenerationService,
     WebApp.Services.Creator.BrandKit.LogoEngine.LogoGenerationService>();
 builder.Services.AddScoped<WebApp.Services.Creator.BrandKit.LogoEngine.ILogoVariationService,
